327e3e6c4001c7a3a8628383ea9e2df92dda6fcf
Jhonnatan Ricardo Semler <jhonnatanricardo@gmail.com>
2020-07-16 19:56:28 -0300

    Change "API" local for Heroku App

--- app/ts/services/NegociacaoService.ts
+++ app/ts/services/NegociacaoService.ts
@@ -2,7 +2,7 @@ import {NegociacaoParcial, Negociacao} from '../models/index';
 
 export class NegociacaoService{
     async obterNegociacoes(handler: HandlerFunction): Promise<Negociacao[]>{
-        return fetch('http://localhost:8080/dados')
+        return fetch('https://api-jrs-negociacoes.herokuapp.com/dados')
             .then(res => handler(res))
             .then(res => res.json())
             .then((dados: NegociacaoParcial[]) => 
